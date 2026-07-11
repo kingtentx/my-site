540ce6bef4d332941f66c17e05d789f859892dfc
kingtentx <kingtentx@users.noreply.github.com>
2026-07-11 21:17:41 +0800

    为招聘增加内容分类

--- src/CIMC.Data/Model/ContentJob.cs
+++ src/CIMC.Data/Model/ContentJob.cs
@@ -5,7 +5,7 @@ using CIMC.Data.ExtModel;
 namespace CIMC.Data
 {
     /// <summary>
-    /// 招聘岗位
+    /// 招聘岗位。
     /// </summary>
     public class ContentJob : ExtFullModifyModel, IActiveModel, ISortModel, IModifyModel
     {
@@ -13,84 +13,43 @@ namespace CIMC.Data
         public int Id { get; set; }
 
         /// <summary>
-        /// 岗位名称
+        /// 招聘分类 ID。
         /// </summary>
+        public int CategoryId { get; set; }
+
         [Required]
         [StringLength(ModelUnits.Len_100)]
         public string JobTitle { get; set; }
 
-        /// <summary>
-        /// 所属部门
-        /// </summary>
         [StringLength(ModelUnits.Len_100)]
         public string Department { get; set; }
 
-        /// <summary>
-        /// 工作地点
-        /// </summary>
         [StringLength(ModelUnits.Len_100)]
         public string WorkLocation { get; set; }
 
-        /// <summary>
-        /// 薪资范围
-        /// </summary>
         [StringLength(ModelUnits.Len_100)]
         public string SalaryRange { get; set; }
 
-        /// <summary>
-        /// 招聘人数
-        /// </summary>
         public int RecruitCount { get; set; } = 1;
 
-        /// <summary>
-        /// 工作类型（全职/兼职/实习）
-        /// </summary>
         [StringLength(ModelUnits.Len_20)]
         public string JobType { get; set; }
 
-        /// <summary>
-        /// 岗位职责（富文本）
-        /// </summary>
         public string Responsibilities { get; set; }
-
-        /// <summary>
-        /// 任职要求（富文本）
-        /// </summary>
         public string Requirements { get; set; }
 
-        /// <summary>
-        /// 联系人
-        /// </summary>
         [StringLength(ModelUnits.Len_50)]
         public string ContactName { get; set; }
 
-        /// <summary>
-        /// 联系电话
-        /// </summary>
         [StringLength(ModelUnits.Len_50)]
         public string ContactPhone { get; set; }
 
-        /// <summary>
-        /// 联系邮箱
-        /// </summary>
         [StringLength(ModelUnits.Len_100)]
         public string ContactEmail { get; set; }
 
-        /// <summary>
-        /// 排序
-        /// </summary>
-        public int Sort { get; set; } = 0;
-
-        /// <summary>
-        /// 是否发布
-        /// </summary>
+        public int Sort { get; set; }
         public bool IsActive { get; set; } = true;
-
-        /// <summary>
-        /// 发布时间
-        /// </summary>
         public DateTime? PublishTime { get; set; }
-
-        public bool IsDelete { get; set; } = false;
+        public bool IsDelete { get; set; }
     }
 }
