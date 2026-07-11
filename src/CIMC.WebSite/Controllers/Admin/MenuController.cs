dd16de215d6955e1aa941fab46652863589ee278
kingtentx <kingtentx@users.noreply.github.com>
2026-07-11 21:21:02 +0800

    隐藏重复导航入口并重命名内容分类

--- src/CIMC.WebSite/Controllers/Admin/MenuController.cs
+++ src/CIMC.WebSite/Controllers/Admin/MenuController.cs
@@ -15,15 +15,11 @@ namespace MySite.Web.Controllers
     [Authorize]
     public class MenuController : AdminBaseController
     {
-        private IPermissionService _permission;
-        private IMapper _mapper;
-        private IRepository<Menu> _menuRepository;
+        private readonly IPermissionService _permission;
+        private readonly IMapper _mapper;
+        private readonly IRepository<Menu> _menuRepository;
 
-        public MenuController(
-            IPermissionService permission,
-            IMapper mapper,
-            IRepository<Menu> menuRepository
-            )
+        public MenuController(IPermissionService permission, IMapper mapper, IRepository<Menu> menuRepository)
         {
             _permission = permission;
             _mapper = mapper;
@@ -36,54 +32,47 @@ namespace MySite.Web.Controllers
             ViewData[PageCode.PAGE_Button_Add] = _permission.CheckPermission(LoginUser, MenuCode.System_Menu, PermissionType.Add);
             ViewData[PageCode.PAGE_Button_Edit] = _permission.CheckPermission(LoginUser, MenuCode.System_Menu, PermissionType.Edit);
             ViewData[PageCode.PAGE_Button_Delete] = _permission.CheckPermission(LoginUser, MenuCode.System_Menu, PermissionType.Delete);
-
             return View();
         }
 
-        /// <summary>
-        /// 列表
-        /// </summary>      
-        /// <returns></returns>
         [HttpGet]
         [PermissionFilter(MenuCode.System_Menu, PermissionType.View)]
         public async Task<JsonResult> GetList()
         {
-
             var result = new ResultModel<List<MenuModel>>();
             var keywords = HttpContext.Request.Query["keywords"];
             var where = LambdaHelper.True<Menu>();
-
-            if (!string.IsNullOrWhiteSpace(keywords))
-                where = where.And(p => p.Title.Contains(keywords));
+            if (!string.IsNullOrWhiteSpace(keywords)) where = where.And(p => p.Title.Contains(keywords));
 
             var query = await _menuRepository.GetListAsync(where, p => p.Sort, true);
             var data = _mapper.Map<List<MenuModel>>(query);
+            foreach (var item in data.Where(p => p.PermissionKey == "Content_ProductCategory")) item.Title = "内容分类";
 
             result.Code = (int)ResultCode.Success;
             result.Message = "成功";
             result.Count = query.Count;
-            result.Data = data.ToList();
-
+            result.Data = data;
             return Json(result);
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
         [HttpGet]
         [PermissionFilter(MenuCode.System_Menu, PermissionType.View)]
         public async Task<JsonResult> GetMenuData()
         {
-            var menulist = await _menuRepository.GetListAsync(p => p.IsShow);
+            // 页面管理已包含导航树配置，后台菜单不再展示重复的导航管理入口。
+            var menulist = await _menuRepository.GetListAsync(p => p.IsShow && p.PermissionKey != "Site_Navigation");
+            var categoryMenu = menulist.FirstOrDefault(p => p.PermissionKey == "Content_ProductCategory");
+            if (categoryMenu != null) categoryMenu.Title = "内容分类";
 
             var treeList = new List<TreeSelectModel>();
             foreach (var parentNode in menulist.Where(t => t.Pid == 0))
             {
-                var model = new TreeSelectModel();
-                model.Id = parentNode.Id;
-                model.Name = parentNode.Title;
-                model.Sort = parentNode.Sort;
+                var model = new TreeSelectModel
+                {
+                    Id = parentNode.Id,
+                    Name = parentNode.Title,
+                    Sort = parentNode.Sort
+                };
                 model.Children = PageUtils.TreeSelect(menulist, model);
                 treeList.Add(model);
             }
@@ -92,31 +81,26 @@ namespace MySite.Web.Controllers
 
         private Dictionary<string, string> GetPermissionType()
         {
-            var result = new ResultModel();
             var buttons = new Dictionary<string, string>();
             foreach (var button in Enum.GetValues(typeof(PermissionType)))
             {
                 buttons.Add(button.ToString(), EnumHelper.GetDescription((PermissionType)button));
             }
-            buttons.Remove(PermissionType.View.ToString());//菜单默认有view权限，不用重复添加
+            buttons.Remove(PermissionType.View.ToString());
             return buttons;
         }
 
         [PermissionFilter(MenuCode.System_Menu, PermissionType.View)]
         public ActionResult Edit(int id, int pid)
         {
-            var model = new MenuModel() { Pid = pid };
+            var model = new MenuModel { Pid = pid };
             if (id > 0)
             {
                 var query = _menuRepository.GetOne(id);
-                if (query != null)
-                {
-                    model = _mapper.Map<MenuModel>(query);
-                }
+                if (query != null) model = _mapper.Map<MenuModel>(query);
             }
             model.PermissionKeys = _permission.GetPermissionKeys();
             model.PermissionTypes = GetPermissionType();
-
             return View(model);
         }
 
@@ -125,7 +109,6 @@ namespace MySite.Web.Controllers
         public async Task<ActionResult> EditPost(int id, MenuModel input)
         {
             var result = new ResultModel();
-
             if (id > 0)
             {
                 if (!_permission.CheckPermission(LoginUser, MenuCode.System_Menu, PermissionType.Edit))
@@ -136,6 +119,12 @@ namespace MySite.Web.Controllers
                 }
 
                 var editmodel = _menuRepository.GetOne(id);
+                if (editmodel == null)
+                {
+                    result.Code = (int)ResultCode.NULL;
+                    result.Message = "菜单不存在";
+                    return Json(result);
+                }
                 editmodel.Pid = input.Pid;
                 editmodel.MenuType = input.MenuType;
                 editmodel.Title = input.Title;
@@ -147,7 +136,6 @@ namespace MySite.Web.Controllers
                 editmodel.IsShow = input.IsShow;
                 editmodel.UpdateBy = LoginUser.UserName;
                 editmodel.UpdateTime = DateTime.Now;
-
                 if (await _menuRepository.UpdateAsync(editmodel))
                 {
                     result.Code = (int)ResultCode.Success;
@@ -162,17 +150,14 @@ namespace MySite.Web.Controllers
                     result.Message = "无操作权限";
                     return Json(result);
                 }
-
                 var model = _mapper.Map<Menu>(input);
                 model.CreationBy = LoginUser.UserName;
-
                 if ((await _menuRepository.AddAsync(model)).Id > 0)
                 {
                     result.Code = (int)ResultCode.Success;
                     result.Message = "添加成功";
                 }
             }
-
             return Json(result);
         }
 
@@ -187,24 +172,11 @@ namespace MySite.Web.Controllers
                 result.Message = "该菜单有子菜单，请先删除子菜单！";
                 return Json(result);
             }
-
             if (await _menuRepository.DeleteAsync(id))
             {
                 result.Code = (int)ResultCode.Success;
                 result.Message = "删除成功！";
             }
-
-            //var query = await _menuRepository.GetQueryable(p => p.Id == id).FirstOrDefaultAsync();
-            //if (query != null)
-            //{
-            //    query.IsDelete = true;
-            //    if (await _menuRepository.UpdateAsync(query))
-            //    {
-            //        result.Code = (int)ResultCode.Success;
-            //        result.Message = "删除成功！";
-            //    }
-            //}
-
             return Json(result);
         }
     }
